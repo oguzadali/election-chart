4cf12ce2eb93977017188a2a4ae0ffba8784766e
oguzadali <ouzadali1@gmail.com>
2021-05-19 09:51:08 +0300

    vote-data-form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,191 @@ namespace election_chart
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxState = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxXP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxYP = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAP = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTP = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxCP = new System.Windows.Forms.TextBox();
+            this.buttonEnterVote = new System.Windows.Forms.Button();
+            this.buttonCharts = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "STATE:";
+            // 
+            // textBoxState
+            // 
+            this.textBoxState.Location = new System.Drawing.Point(159, 63);
+            this.textBoxState.Name = "textBoxState";
+            this.textBoxState.Size = new System.Drawing.Size(200, 22);
+            this.textBoxState.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "X PARTY:";
+            // 
+            // textBoxXP
+            // 
+            this.textBoxXP.Location = new System.Drawing.Point(159, 101);
+            this.textBoxXP.Name = "textBoxXP";
+            this.textBoxXP.Size = new System.Drawing.Size(200, 22);
+            this.textBoxXP.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Y PARTY:";
+            // 
+            // textBoxYP
+            // 
+            this.textBoxYP.Location = new System.Drawing.Point(159, 139);
+            this.textBoxYP.Name = "textBoxYP";
+            this.textBoxYP.Size = new System.Drawing.Size(200, 22);
+            this.textBoxYP.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "A PARTY:";
+            // 
+            // textBoxAP
+            // 
+            this.textBoxAP.Location = new System.Drawing.Point(159, 176);
+            this.textBoxAP.Name = "textBoxAP";
+            this.textBoxAP.Size = new System.Drawing.Size(200, 22);
+            this.textBoxAP.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "T PARTY:";
+            // 
+            // textBoxTP
+            // 
+            this.textBoxTP.Location = new System.Drawing.Point(159, 214);
+            this.textBoxTP.Name = "textBoxTP";
+            this.textBoxTP.Size = new System.Drawing.Size(200, 22);
+            this.textBoxTP.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "C PARTY:";
+            // 
+            // textBoxCP
+            // 
+            this.textBoxCP.Location = new System.Drawing.Point(159, 251);
+            this.textBoxCP.Name = "textBoxCP";
+            this.textBoxCP.Size = new System.Drawing.Size(200, 22);
+            this.textBoxCP.TabIndex = 1;
+            // 
+            // buttonEnterVote
+            // 
+            this.buttonEnterVote.Location = new System.Drawing.Point(159, 292);
+            this.buttonEnterVote.Name = "buttonEnterVote";
+            this.buttonEnterVote.Size = new System.Drawing.Size(200, 46);
+            this.buttonEnterVote.TabIndex = 2;
+            this.buttonEnterVote.Text = "ENTER VOTE DATA";
+            this.buttonEnterVote.UseVisualStyleBackColor = true;
+            // 
+            // buttonCharts
+            // 
+            this.buttonCharts.Location = new System.Drawing.Point(159, 354);
+            this.buttonCharts.Name = "buttonCharts";
+            this.buttonCharts.Size = new System.Drawing.Size(88, 46);
+            this.buttonCharts.TabIndex = 2;
+            this.buttonCharts.Text = "CHARTS";
+            this.buttonCharts.UseVisualStyleBackColor = true;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(268, 354);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(91, 46);
+            this.buttonExit.TabIndex = 2;
+            this.buttonExit.Text = "EXIT";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(544, 479);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonCharts);
+            this.Controls.Add(this.buttonEnterVote);
+            this.Controls.Add(this.textBoxCP);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxTP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxAP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxYP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxXP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxState);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Election Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxState;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxXP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxYP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTP;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxCP;
+        private System.Windows.Forms.Button buttonEnterVote;
+        private System.Windows.Forms.Button buttonCharts;
+        private System.Windows.Forms.Button buttonExit;
     }
 }
 
